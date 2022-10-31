f8b79d6b08a75012cbd1a64ff15fff0f8137ba1d
deivid212 <115495220+deivid212@users.noreply.github.com>
2022-10-31 14:20:33 -0300

    materia escolar

---
+++ MaterialEscolar/Models/apontador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Models
+{
+    public class Apontador
+    {
+        private double tamanho;
+        private string cor;
+        private string material;
+        private double dureza;
+
+
+        public double Tamanho
+        {
+            get { return tamanho; }
+            set { tamanho = value; }
+        }
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+        public double Dureza
+        {
+            get { return dureza; }
+            set { dureza = value; }
+        }
+
+        public Apontador(double _tamanho, string _cor, string _material)
+        {
+            Tamanho = _tamanho;
+            Cor = _cor;
+            Material = _material;
+            Dureza = 100;
+        }
+        public void Apontar()
+        {
+            dureza -= 2;
+        }
+        public void Exibir()
+        {
+            Console.WriteLine("Cor: " + cor);
+            Console.WriteLine("Tamanho: " + tamanho);
+            Console.WriteLine("Meterial: " + Material);
+            Console.WriteLine("Dureza: " + dureza);
+        }
+    }
+}
---
+++ MaterialEscolar/Models/borracha.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Borracha
+    {
+        private double dureza;
+        private double tamanho;
+        private string material;
+
+        public double Dureza
+        {
+            get { return dureza; }
+            set { dureza = value; }
+        }
+        public double Tamanho
+        {
+            get { return tamanho; }
+            set { tamanho = value; }
+        }
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+
+        public Borracha(double _tamanho, string _material)
+        {
+            Dureza = 100;
+            Tamanho = _tamanho;
+            Material = _material;
+        }
+        public void Exibir()
+        {
+            Console.WriteLine("Dureza: " + Dureza);
+            Console.WriteLine("Tamanho: " + Tamanho);
+            Console.WriteLine("Material: " + Material);
+        }
+        public void Apagar()
+        {
+            dureza -= 2;
+        }
+    }
+}
